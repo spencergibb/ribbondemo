859a75939fb2459e77f8ceac42aabb291084374c
Spencer Gibb <spencer@gibb.us>
2015-09-10 16:05:36 -0600

    removed main from properties

--- src/main/java/demo/DemoProperties.java
+++ src/main/java/demo/DemoProperties.java
@@ -1,8 +1,8 @@
 package demo;
 
 import lombok.Data;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.util.PatternMatchUtils;
 
 /**
  * @author Spencer Gibb
@@ -11,18 +11,4 @@ import org.springframework.util.PatternMatchUtils;
 @Data
 public class DemoProperties {
 	String filterPattern;
-
-	public static void main(String[] args) {
-	    String[] ids = new String[] {
-				"localhost:9091",
-				"localhost2:9092",
-				"localhost3:9093"
-		};
-
-		String pattern = "*2:*";
-		for (String id : ids) {
-			boolean matches = PatternMatchUtils.simpleMatch(pattern, id);
-			System.out.println(id + " matches " + pattern + ": " + matches);
-		}
-	}
 }
